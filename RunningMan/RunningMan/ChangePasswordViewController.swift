4f7f7cf91f1dc6e706a6477b72745f27e3924773
ZHAI ZHE <e0013348@u.nus.edu>
2016-05-05 20:25:52 +0800

    commit the initial project

---
+++ RunningMan/RunningMan/ChangePasswordViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ChangePasswordViewController.swift
+//  RunningMan
+//
+//  Created by zz on 5/5/16.
+//  Copyright © 2016 Group16. All rights reserved.
+//
+
+import UIKit
+
+class ChangePasswordViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
