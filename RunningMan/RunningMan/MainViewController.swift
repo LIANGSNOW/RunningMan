faec2fa5fd90d0896cb04b91ea514153c9e2e540
mac <mac@macs-mac.local>
2016-05-06 10:36:46 -0700

    location tracking

--- RunningMan/RunningMan/MainViewController.swift
+++ RunningMan/RunningMan/MainViewController.swift
@@ -7,14 +7,84 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController ,CLLocationManagerDelegate,MKMapViewDelegate	{
+    
+    @IBOutlet weak var mapTrack: MKMapView!
+     @IBOutlet weak var theLabel: UILabel!
 
+    var locationManager:CLLocationManager!
+    var myLocations:[CLLocation] = []
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        mapTrack.delegate = self
+        mapTrack.mapType = MKMapType(rawValue: 0)!
+        mapTrack.showsUserLocation = true
+        mapTrack.userTrackingMode = MKUserTrackingMode(rawValue: 2)!
+       // theLabel.text = "1321321"
+        
+
     }
+    func locationManager(manager: CLLocationManager!, didUpdateToLocation newLocation: CLLocation!, fromLocation oldLocation: CLLocation!) {
+        print("111")
+        if let oldLocationNew = oldLocation as CLLocation?{
+            let oldCoordinates = oldLocationNew.coordinate
+            let newCoordinates = newLocation.coordinate
+            var area = [oldCoordinates, newCoordinates]
+            var polyline = MKPolyline(coordinates: &area, count: area.count)
+            mapTrack.addOverlay(polyline)
+        }
+        annotateMap(newLocation.coordinate)
+       
+         print("222")
+    }
+  
+    
+    func annotateMap(newCoordinate: CLLocationCoordinate2D){
+        let latDelta:CLLocationDegrees = 0.01
+        let longDelta: CLLocationDegrees = 0.01
+        let theSpan:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        
+        let myLocation:CLLocationCoordinate2D = newCoordinate
+        let theRegion:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, theSpan)
+        self.mapTrack.setRegion(theRegion, animated: true)
+        self.mapTrack.mapType = MKMapType.Standard
+        
+       /* let myHomePin = MKPointAnnotation()
+        myHomePin.coordinate = newCoordinate
+        myHomePin.title = "I am here"
+        self.mapTrack.addAnnotation(myHomePin)*/
+    }
+  /*
+    func mapView(mapView: MKMapView, viewForOverlay overlay: MKOverlay) -> MKOverlayView {
+        
+        if overlay is MKPolyline {
+        }
+        return nil
+
+    }
+  */
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        
+        if overlay is MKPolyline {
+            var polylineRenderer = MKPolylineRenderer(overlay: overlay)
+            polylineRenderer.strokeColor = UIColor.blueColor()
+            polylineRenderer.lineWidth = 4
+            return polylineRenderer
+        }
+        return nil
+    }
+ 
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
